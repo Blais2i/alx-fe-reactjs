0d1b4532eaae8a0dbdce29699490bec967cfe118
Blaise Iradukunda <blaisealkado@gmail.com>
2025-10-04 05:07:43 -0700

    feat: make UserProfile component fully responsive with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -3,7 +3,7 @@ import UserProfile from './components/UserProfile'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 py-8">
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 py-4 sm:py-6 md:py-8 px-4 sm:px-6">
       <UserProfile />
     </div>
   )
